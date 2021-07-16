7f91239e82d51fba1b8773e8b3b4b9556a236510
vigneshm670 <foundingtitan1512@gmail.com>
2021-07-16 21:29:55 +0530

    file08.c committed

---
+++ file08.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct struct01
+{
+    char a[50];
+    int rollno;
+    char b[50];
+}; 
+
+void main()
+{ 
+    struct struct01 obj1,obj2;
+    printf("Enter the name......");
+    scanf("%s\n",obj1.a);
+    printf("%s\n",obj1.a);
+}
+
